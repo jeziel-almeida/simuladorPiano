e4e69ed90142416cd84d1ff0ba86487b26a82605
Jeziel Almeida <jeziel.almeida16@gmail.com>
2025-05-18 22:59:46 -0300

    Adição de áudio para as teclas do piano

---
+++ src/scripts/engine.js
@@ -0,0 +1,15 @@
+const pianoKeys = document.querySelectorAll(".key");
+
+let audio = new Audio("src/tunes/a.wav")
+
+const playTune = (key) => {
+    audio.src = `src/tunes/${key}.wav`;
+    audio.play();
+}
+
+pianoKeys.forEach((key) => {
+    console.log(key.dataset.key)
+    key.addEventListener("click", () => {
+        playTune(key.dataset.key);
+    })
+})
